4fbaf68629823ae8eb7cc3f0cc0b2ddc221c0aa2
Veronica Yung <71048282+veronicaYung@users.noreply.github.com>
2025-02-15 14:55:12 -0500

    landing page completed

--- my-react-app/src/Pages/LandingPage.jsx
+++ my-react-app/src/Pages/LandingPage.jsx
@@ -1,16 +1,28 @@
+//LandingPage.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import '../style/landing-page.css';
+import { useNavigate } from 'react-router-dom';
+import landingImg from '../assets/LandingImg.jpg'; 
 
 const LandingPage = () => {
-  return (
-    <div>
-      <h1>Landing Page</h1>
-      <nav>
-        <Link to="/connect">Connct Page</Link>
-      </nav>
-    </div>
+    const navigate = useNavigate();
 
-    
+    const handleConnectClick = () => {
+      navigate('/connect');
+    };
+  
+  return (
+    <div className="landing-page">
+      <div className="left-side">
+        <h1>Socialize with <br></br> Silver Circle</h1>
+        <p>
+          Our application connects elders together, helping them socialize and avoid loneliness.
+        </p>
+        <button onClick={handleConnectClick} className="connect-button">Connect Now</button>
+      </div>
+      <div className="right-side">
+      </div>
+    </div>
   );
 };
 
