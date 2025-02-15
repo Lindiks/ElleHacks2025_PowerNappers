dab9f250dbf698579a789ebb9a30e5a45e4c523f
Rehatpreet kaur <rehatpreetkaur981@gmail.com>
2025-02-15 17:31:14 -0500

    Code to be pushed

--- my-react-app/src/Pages/ConnectPage.jsx
+++ my-react-app/src/Pages/ConnectPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Tags from '../Components/Tags.jsx';
+import SideBar from '../Components/SideBar.jsx';
+import "../style/ContentPage.css"
 
 const ConnectPage = () => {
   return (
-    <div>
+    <div className='page-container'>
+      <SideBar/>
+      <div className="content-panel">
+
       <h1>Connect Page</h1>
       <p>This is the Connect Page.</p>
       <Tags></Tags>
+      </div>
     </div>
   );
 };
