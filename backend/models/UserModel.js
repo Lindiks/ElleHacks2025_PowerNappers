11de2c4dc3a491c9abfd9492ddfb0c5b908a7810
Michelle Murphy <73407646+michellemurphy529@users.noreply.github.com>
2025-02-15 20:51:48 -0500

    Update UserModel.js
    need to add address and city

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -4,39 +4,14 @@ const Schema = mongoose.Schema;
 
 let userSchema = Schema({
     _id: ObjectId,
-    Username: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 30,
-        // match: /[A-Za-Z]+/, //no special characters
-        trim: true          //removes whitespace from both ends of the string
-    },
-        
-    Password:{
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 30,
-        // match: /[A-Za-Z]+/, 
-        trim: true          
-    },
-    Name: String, 
-    Age: Number,  //put a catch to only allow betweeen certain age 
-    Assistance: Boolean, // If a nanny/assitant is required 
-    LimitedMobility: Boolean, // Limited mobility 
-    Description: String,
+    FirstName: String, 
+    LastName: String,
+    Age: Number,
+    Assistance: Boolean,
+    LimitedMobility: Boolean,
+    Availability: [String],
     Interests: [String],
-    Friends: [{type: Schema.Types.ObjectId, ref: 'User'}], 
-    
-    History: [{type: Schema.Types.ObjectId, ref: 'Hangout'}]
+    Bio: String
 })
-
-
-
-userSchema.statics.findByUsername = function(name, callback){
-    this.findOne({Username: name}, callback);
-}
-
 
 module.exports = mongoose.model("User", userSchema);
