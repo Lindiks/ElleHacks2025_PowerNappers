b96e6e56a1ef033d77aa417ece47d4f37b3c636e
Michelle Murphy <73407646+michellemurphy529@users.noreply.github.com>
2025-02-15 20:48:26 -0500

    Update create-db.js
    upstream issues

--- backend/create-db.js
+++ backend/create-db.js
@@ -1,41 +1,34 @@
-const fileName = "./exampledata.json"; 
-
+const fileName = "./userData.json"; 
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
 const User = require("./models/UserModel");
-
 let allUsers = [];
 
-//Read the JSON data provided in the given file
-// Create new users with this 
+//Read the JSON data for "users"
 let data = require(fileName);
+
+//Add all "users"
 data.forEach(u=>{
     console.log(u)
     let newUser = new User();
-    // newUser._id = new mongoose.Types.ObjectId();
-    newUser._id = u._id
-    newUser.Username = u.Username
-    newUser.Password = u.Password
-    newUser.Name = u.Name
+    newUser._id = new mongoose.Types.ObjectId();
+    // newUser._id = u._id
+    newUser.FirstName = u.FirstName
+    newUser.LastName = u.LastName
     newUser.Age = u.Age
     newUser.Assistance = u.Assistance
     newUser.LimitedMobility = u.LimitedMobility
-    newUser.Description = u.Description
-    newUser.Interests = u.Interests
-    newUser.Friends = []
-    newUser.History = []
+    newUser.Availability = []
+    newUser.Interests = []
+    newUser.Bio = u.Bio
 
   allUsers.push(newUser)
 })
 
 mongoose.connect('mongodb+srv://admin:admin@silvercircledb.ka6ob.mongodb.net/SilverCircle', {useNewUrlParser: true});
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error'));
+
+//Add all our "users"
 db.once('open', function() {
-  //We are connected. Drop the database first so we start fresh each time.
-// mongoose.connection.db.dropDatabase(function(err, result){})
-User.insertMany(allUsers)
-      });
-    // });
-  //});
+  User.insertMany(allUsers)
+});
